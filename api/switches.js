68724d582bbf3aa0b7316dc0fc28a799eec1e6ee
Victor Cabieles <victorcabieles@gmail.com>
2017-10-26 23:58:31 -0400

    some weird things are happnening

--- api/switches.js
+++ api/switches.js
@@ -6,27 +6,27 @@ var led = new Gpio(14, 'out');
 router.post("/on", (req, res, next)=>{
     let body = req.body;
 
-    var onOrOFF = 0, count = 0;
-    var theInterval = setInterval(()=>{
-        count++;
-        console.log(count);
-        if(count >= 50){
-            led.unexport();
-            clearInterval(theInterval);
-            console.log("turning off system");
-        }else{
-            console.log("count not 50");
-            if(onOrOFF === 0){
-                onOrOFF = 1;
-                console.log("turning on");
-                led.writeSync(1);
-            }else{
-                onOrOFF = 0;
-                console.log("turning off");
-                led.writeSync(0)
-            }
-        }
-    },500);
+    // var onOrOFF = 0, count = 0;
+    // var theInterval = setInterval(()=>{
+    //     count++;
+    //     console.log(count);
+    //     if(count >= 50){
+    //         led.unexport();
+    //         clearInterval(theInterval);
+    //         console.log("turning off system");
+    //     }else{
+    //         console.log("count not 50");
+    //         if(onOrOFF === 0){
+    //             onOrOFF = 1;
+    //             console.log("turning on");
+    //             led.writeSync(1);
+    //         }else{
+    //             onOrOFF = 0;
+    //             console.log("turning off");
+    //             led.writeSync(0)
+    //         }
+    //     }
+    // },500);
     res.status(200).json({
         status: "I think is on?!",
         transaction: "PAID",
